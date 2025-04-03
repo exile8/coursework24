b886fd59db7eaf414cf806e0019885e8208642e9
Yuliya <pjulie717119@gmail.com>
2025-04-03 17:29:18 +0500

    Current implementation of shap with gradient explainer

--- audiointerp/interpretation/shap.py
+++ audiointerp/interpretation/shap.py
@@ -6,29 +6,25 @@ class SHAPInterpreter(BaseInterpreter):
 
     def __init__(self, model, background_data):
         super().__init__(model)
-        self.explainer = shap.Explainer(self.model)
-
-        background_data = background_data.to("cpu").detach().numpy()
         
-        self.explainer = shap.KernelExplainer(self.model_forward, background_data)
-
-    def model_forward(self, inputs):
-        inputs = torch.tensor(inputs).to(self.device)
-        with torch.no_grad():
-            logits = self.model(inputs)
-        return logits.cpu().numpy()
+        self.explainer = shap.GradientExplainer(self.model, background_data)
 
     def compute_interpretation(self, inputs):
-        inputs = inputs.to(self.device).requires_grad_(True)
-        inputs_np = inputs.detach().cpu().numpy()
-        
-        logits = self.model(inputs)
+        inputs = inputs.to(self.device)
+
+        with torch.no_grad():
+            logits = self.model(inputs)
         predicted_class = logits.argmax(dim=1).cpu().numpy()
 
-        shap_values = self.explainer(inputs_np)
-        attributions = torch.tensor(shap_values.values).to(self.device)
+        shap_values = self.explainer.shap_values(inputs)
 
-        attributions = attributions.unsqueeze(1)
+        batch_size = inputs.size(0)
+        attributions_list = []
+        for i in range(batch_size):
+            cls = predicted_class[i]
+            attributions_list.append(shap_values[cls][i])
+
+        attributions = torch.tensor(attributions_list, device=self.device)
 
         return attributions
 
