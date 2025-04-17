4f6466108f1598152c01412553e2bffaf1d7f745
Yuliya <pjulie717119@gmail.com>
2025-04-17 18:28:16 +0500

    Debug shap interpreter

--- audiointerp/interpretation/shap.py
+++ audiointerp/interpretation/shap.py
@@ -7,7 +7,7 @@ class SHAPInterpreter(BaseInterpreter):
     def __init__(self, model, background_data):
         super().__init__(model)
         
-        self.explainer = shap.GradientExplainer(self.model, background_data)
+        self.explainer = shap.DeepExplainer(self.model, background_data)
 
     def compute_interpretation(self, inputs):
         inputs = inputs.to(self.device)
@@ -16,15 +16,17 @@ class SHAPInterpreter(BaseInterpreter):
             logits = self.model(inputs)
         predicted_class = logits.argmax(dim=1).cpu().numpy()
 
-        shap_values = self.explainer.shap_values(inputs)
+        shap_values = self.explainer.shap_values(inputs, ranked_outputs=1, output_rank_order='max', check_additivity=False)
+        #print(shap_values)
 
-        batch_size = inputs.size(0)
-        attributions_list = []
-        for i in range(batch_size):
-            cls = predicted_class[i]
-            attributions_list.append(shap_values[cls][i])
+        #batch_size = inputs.size(0)
+        #attributions_list = []
+        #for i in range(batch_size):
+        #    cls = predicted_class[i]
+        #    attributions_list.append(shap_values[cls][i])
 
-        attributions = torch.tensor(attributions_list, device=self.device)
+        attributions = torch.from_numpy(shap_values[0][..., 0]).to(device=self.device, dtype=torch.float32)
+        attributions = torch.clamp(attributions, min=0.)
 
         return attributions
 
