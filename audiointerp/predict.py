758d0dbb7efa5973aa22f46540ce412bd25644b1
Yuliya <pjulie717119@gmail.com>
2025-05-07 15:53:28 +0500

    Refactor predict: separate steps for attr computations and masking metrics

--- audiointerp/predict.py
+++ audiointerp/predict.py
@@ -7,6 +7,9 @@ import torch.nn.functional as F
 import librosa
 import numpy as np
 import pandas as pd
+import gc
+from collections import defaultdict
+
 
 class Predict:
 
@@ -78,7 +81,7 @@ class Predict:
 
 
         n_mels = inputs.shape[2]
-        mel_freqs = librosa.mel_frequencies(n_mels=n_mels, fmin=0, fmax=sr / 2)
+        mel_freqs = librosa.mel_frequencies(n_mels=n_mels, fmin=50, fmax=14000)
 
         plt.figure(figsize=(6, 4))
         original_spec = inputs[0].squeeze().detach().cpu().numpy()
@@ -109,53 +112,97 @@ class Predict:
         plt.close()
 
         return results
-        
 
-    def predict_set(self, dataloader, results_csv_name, save_dir="results"):
-        results = {
-            "FF": [],
-            "AI": [],
-            "AD": [],
-            "AG": [],
-            "FidIn": [],
-            "SPS": [],
-            "COMP": []
-        }
 
-        for wavs, y in dataloader:
+    def compute_and_save_interpretations_set(self, dataloader, save_path, start_from_sample=1):
+        os.makedirs(save_path, exist_ok=True)
+
+        for iter_num, (wavs, y) in enumerate(dataloader, start=1):
+
+            if iter_num < start_from_sample:
+                continue
+
+            if iter_num % 50 == 0:
+                del self.interpretator
+                gc.collect()
+                torch.cuda.empty_cache()
+                self.interpretator = self.interp_method_cls(self.model, **self.interp_method_kwargs)
+
             wavs = wavs.to(self.device)
             inputs, *_ = self.feature_extractor(wavs)
-            _, masks = self.interpretator.interpret(inputs).values()
+            attrs = self.interpretator.interpret(inputs, ret_masks=False)
 
-            unmasked_inputs = (inputs - inputs.amin(dim=(1, 2, 3), keepdim=True)) * (1 - masks) + inputs.amin(dim=(1, 2, 3), keepdim=True)
-            masked_inputs = (inputs - inputs.amin(dim=(1, 2, 3), keepdim=True)) * masks + inputs.amin(dim=(1, 2, 3), keepdim=True)
+            fn = os.path.join(save_path, f'iter_{iter_num}.pt')
+            torch.save(attrs, fn)
+        
 
-            with torch.no_grad():
-                probs_original = F.softmax(self.model(inputs), dim=1)
-                probs_masked = F.softmax(self.model(masked_inputs), dim=1)
-                probs_unmasked = F.softmax(self.model(unmasked_inputs), dim=1)
+    def predict_set(self, dataloader, results_csv_name, save_dir="results", compute_first=True, start_from_sample=1):
+        results = defaultdict(lambda: defaultdict(list))
 
-            ff = Metrics.compute_FF(probs=probs_original, probs_out=probs_unmasked)
-            ai = Metrics.compute_AI(probs=probs_original, probs_in=probs_masked)
-            ad = Metrics.compute_AD(probs=probs_original, probs_in=probs_masked)
-            ag = Metrics.compute_AG(probs=probs_original, probs_in=probs_masked)
-            fidin = Metrics.compute_FidIn(probs=probs_original, probs_in=probs_masked)
-            sps = Metrics.compute_SPS(inputs, masks, probs_original, self.device)
-            comp = Metrics.compute_COMP(inputs, masks, probs_original, self.device)
+        if not os.path.isdir(save_dir):
+            os.makedirs(save_dir)
 
-            results["FF"].append(ff.cpu().view(-1))
-            results["AI"].append(ai.cpu().view(-1))
-            results["AD"].append(ad.cpu().view(-1))
-            results["AG"].append(ag.cpu().view(-1))
-            results["FidIn"].append(fidin.cpu().view(-1))
-            results["SPS"].append(torch.tensor(sps).cpu().view(-1))
-            results["COMP"].append(torch.tensor(comp).cpu().view(-1))
+        name, _ = os.path.splitext(results_csv_name)
+        path_to_attrs = os.path.join(save_dir, name)
+        
+        if not os.path.isdir(path_to_attrs):
+            os.makedirs(path_to_attrs)
+
+        if compute_first:
+            self.compute_and_save_interpretations_set(dataloader, path_to_attrs, start_from_sample)
+
+        for iter_num, (wavs, y) in enumerate(dataloader, start=1):
+
+            fn = os.path.join(path_to_attrs, f'iter_{iter_num}.pt')
+            interpretations = torch.load(fn).to(self.device)
+
+            wavs = wavs.to(self.device)
+            inputs, *_ = self.feature_extractor(wavs)
+            _, masks = self.interpretator.interpret(inputs, interpretations=interpretations, ret_masks=True)
+
+            for mask_type, m in masks.items():
+
+                unmasked_inputs = (inputs - inputs.amin(dim=(-3, -2, -1), keepdim=True)) * (1 - m) + inputs.amin(dim=(-3, -2, -1), keepdim=True)
+                masked_inputs = (inputs - inputs.amin(dim=(-3, -2, -1), keepdim=True)) * m + inputs.amin(dim=(-3, -2, -1), keepdim=True)
+
+                with torch.no_grad():
+                    probs_original = F.softmax(self.model(inputs), dim=1)
+                    probs_masked = F.softmax(self.model(masked_inputs), dim=1)
+                    probs_unmasked = F.softmax(self.model(unmasked_inputs), dim=1)
+
+                _, preds = torch.max(probs_original, dim=1)
+
+                ff = Metrics.compute_FF(probs=probs_original, probs_out=probs_unmasked)
+                ai = Metrics.compute_AI(probs=probs_original, probs_in=probs_masked)
+                ad = Metrics.compute_AD(probs=probs_original, probs_in=probs_masked)
+                ag = Metrics.compute_AG(probs=probs_original, probs_in=probs_masked)
+                fidin = Metrics.compute_FidIn(probs=probs_original, probs_in=probs_masked)
+                sps = Metrics.compute_SPS(inputs, interpretations, probs_original, self.device)
+                comp = Metrics.compute_COMP(inputs, interpretations, probs_original, self.device)
+
+                results[mask_type]["FF"].append(ff.cpu().view(-1))
+                results[mask_type]["AI"].append(ai.cpu().view(-1))
+                results[mask_type]["AD"].append(ad.cpu().view(-1))
+                results[mask_type]["AG"].append(ag.cpu().view(-1))
+                results[mask_type]["FidIn"].append(fidin.cpu().view(-1))
+                results[mask_type]["SPS"].append(torch.tensor(sps).cpu().view(-1))
+                results[mask_type]["COMP"].append(torch.tensor(comp).cpu().view(-1))
+                results[mask_type]["is_correct"].append((preds.cpu() == y).view(-1))
 
     
-        for m in results:
-            results[m] = torch.cat(results[m], dim=0)
+        for mask_type in results:
+            for metric in results[mask_type]:
+                results[mask_type][metric] = torch.cat(results[mask_type][metric], dim=0)
 
-        results_df = pd.DataFrame(results)
+        frames = []
+        for mask_type, metrics in results.items():
+            df_tmp = pd.DataFrame({(mask_type, m): v for m, v in metrics.items()})
+            frames.append(df_tmp)
+
+        results_df = pd.concat(frames, axis=1)
+        results_df.columns = pd.MultiIndex.from_tuples(results_df.columns)
+        results_df.index.name = "sample"
+
         results_path = os.path.join(save_dir, results_csv_name)
         if not os.path.isdir(save_dir):
             os.makedirs(save_dir)
